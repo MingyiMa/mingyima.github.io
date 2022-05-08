419f4472881a484cddae430817835cf9ae21d5c7
MingyiMa <steve.ma2080@gmail.com>
2022-05-07 23:28:55 -0400

    - minor

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -32,6 +32,7 @@ release = '0.1'
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
